c80873423432d5b15249c20ca499d7e33663d464
blakehawkins <blake.hawkins.11@gmail.com>
2021-12-23 01:31:19 +0000

    search demo

---
+++ generate_tinysearch_index.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+mkdir -p tmp
+
+ls -1 _pages/ | \
+xargs -I {} bash -c "pandoc _pages/{} --to plain | pandoc --from html --to plain | jq -R '[inputs] | add' > tmp/{}"
+
+pushd tmp
+jq -n 'reduce inputs as $s (.; . + [{title: input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), url: input_filename | gsub("_"; "-") | sub("./(?<val>.*).md"; "https://fresnograpestakeyard.com/products/\(.val)"), body: [input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), " ", $s] | add}])' ./* > tinysearch.json
+popd
+
+tinysearch tmp/tinysearch.json
