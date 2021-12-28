35e5c2b87edf97fccdf129afe8e5fe6247a8f743
blakehawkins <blake.hawkins.11@gmail.com>
2021-12-28 02:39:54 +0000

    search box

--- generate_tinysearch_index.sh
+++ generate_tinysearch_index.sh
@@ -8,7 +8,7 @@ ls -1 _pages/ | \
 xargs -I {} bash -c "pandoc _pages/{} --to plain | pandoc --from html --to plain | jq -R '[inputs] | add' > tmp/{}"
 
 pushd tmp
-jq -n 'reduce inputs as $s (.; . + [{title: input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), url: input_filename | gsub("_"; "-") | sub("./(?<val>.*).md"; "https://fresnograpestakeyard.com/products/\(.val)"), body: [input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), " ", $s] | add}])' ./* > tinysearch.json
+jq -n 'reduce inputs as $s (.; . + [{title: input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), url: input_filename | gsub("_"; "-") | sub("./(?<val>.*).md"; "/products/\(.val)"), body: [input_filename | gsub("_"; " ") | sub("./(?<val>.*).md"; "\(.val)"), " ", $s] | add}])' ./* > tinysearch.json
 popd
 
 tinysearch tmp/tinysearch.json
